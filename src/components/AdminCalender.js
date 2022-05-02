52edd6bf1266c5f7cf85c9d3495d75a4738822fa
Wxmnath <Wxmnath@aol.com>
2022-05-02 19:28:01 +0100

    Calender added to AdminCalender

--- src/components/AdminCalender.js
+++ src/components/AdminCalender.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { ViewState } from "@devexpress/dx-react-scheduler";
+import {
+  DateNavigator,
+  TodayButton,
+  WeekView,
+  Toolbar,
+  Appointments,
+  Scheduler,
+} from "@devexpress/dx-react-scheduler-material-ui";
 
 function AdminCalender() {
-  return <div>AdminCalender</div>;
+  const schedulerData = [
+    {
+      startDate: "2022-05-02T09:00",
+      endDate: "2022-05-02T10:00",
+      title: "Sully",
+    },
+    {
+      startDate: "2022-05-03T12:00",
+      endDate: "2022-05-03T13:00",
+      title: "Buddy",
+    },
+  ];
+
+  return (
+    <div id="AdminCalender">
+      <Scheduler data={schedulerData}>
+        <ViewState />
+        <WeekView startDayHour={8} endDayHour={18} />
+        <Toolbar />
+        <DateNavigator />
+        <TodayButton />
+        <Appointments />
+      </Scheduler>
+    </div>
+  );
 }
 
 export default AdminCalender;
